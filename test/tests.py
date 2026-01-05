6ee58b3be64bbb01ef5fd07c0b31557cf14bf335
efecotuk <efe.cotuk@std.yeditepe.edu.tr>
2026-01-05 22:05:47 +0300

    Update tests.py

--- test/tests.py
+++ test/tests.py
@@ -1,1 +1,22 @@
+from bank_operations import deposit_money, withdraw_money
 
+def test_deposit_money():
+    user = {
+        "balance": 100.0,
+        "transactions": []
+    }
+
+    deposit_money(user, 50)
+    assert user["balance"] == 150.0
+
+
+def test_withdraw_money():
+    user = {
+        "balance": 100.0,
+        "transactions": []
+    }
+
+    withdraw_money(user, 40)
+    assert user["balance"] == 60.0
+
+#Basic tests
