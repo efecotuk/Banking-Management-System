6ff58e9095980477b5c9c65f5be7f056a3639588
efecotuk <efe.cotuk@std.yeditepe.edu.tr>
2026-01-05 23:46:24 +0300

    Update file_manager.py

--- file_manager.py
+++ file_manager.py
@@ -6,10 +6,12 @@ def load_users_from_file(path):
         return {}
     with open(path, "r") as f:
         return json.load(f)
+# Reads user data from a JSON file and returns it as a dictionary. 
 
 def save_users_to_file(path, users):
     with open(path, "w") as f:
         json.dump(users, f, indent=4)
-
+# Writes the current users dictionary to a JSON file. 
+# (I hate JSON files so much..)
 
 
